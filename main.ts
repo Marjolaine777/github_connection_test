3338bc7eb0712ffc8aa4ac4a018807452e6d1423
Marjolaine777 <66908418+Marjolaine777@users.noreply.github.com>
2021-07-16 07:41:45 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,3 +8,8 @@ for (let index = 0; index < 4; index++) {
         `)
     basic.showNumber(7)
 }
+led.setBrightness(90)
+for (let index = 0; index < 4; index++) {
+    basic.showIcon(IconNames.Heart)
+    basic.showString("Github rocks!")
+}
